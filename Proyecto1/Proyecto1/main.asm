18842c37102ea522401c9a4b38ffbc4d3cd3dcf0
alma-mata <mat23038@uvg.edu.gt>
2025-03-04 17:50:47 -0600

    Creación de proyecto
    Creación de los documentos del proyecto. Solo se agregan comentarios y se establece la estructura general del programa.

---
+++ Proyecto1/Proyecto1/main.asm
@@ -0,0 +1,28 @@
+;-----------------------------------------------
+; Universidad del Valle de Guatemala
+; IE2023: Programacion de Microcontroladores
+; Proyecto1.asm
+
+; Autor: Alma Lisbeth Mata Ixcayau
+; Proyecto: Proyecto 1, reloj digital
+; Descripcion: Reloj digital que muestre la hora y fecha.
+; Hardware: ATMEGA328P
+; Creado: 04/03/2025
+; Ultima modificacion: [pendiente]
+;-----------------------------------------------
+
+//----------------- ENCABEZADO -----------------
+.include "M328PDEF.inc"
+// Variables SRAM
+
+// Codigo FLASH
+
+// Variables globales (Registros)
+
+//--------- Tablas ---------
+
+SETUP:
+
+MAIN:
+
+//--------------- SUB-RUTINAS ---------------
