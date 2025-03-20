4c12e4ab083192df4fc6a4cfff20c97f5e85b959
alma-mata <mat23038@uvg.edu.gt>
2025-03-20 17:24:42 -0600

    Proyecto terminado (creo)
    Se arreglan algunos errores en la configuración de fecha. La configuración de la alarma funciona correctamente. No hay más por agregar.

--- Proyecto1/Proyecto1/main.asm
+++ Proyecto1/Proyecto1/main.asm
@@ -19,18 +19,17 @@
 .equ	T0VALUE = 131
 .equ	T1VALUEH = 0x0B
 .equ	T1VALUEL = 0xDC		//0x0BDC = 3036
-.equ	T2VALUE = 131
-.equ	max_ciclosT1 = 1
+.equ	max_ciclosT1 = 120
 // Variables globales (Registros)
-.def	out_PORTD = R2
-.def	in_PORTC = R3
-.def	out_MODO = R4		// Salida para PC4 y PC5
-.def	CONTADOR7 = R5
-.def	SALIDA7 = R6
-.def	max_dia = R7
-.def	contador_dia = R8
-.def	puntos_LED = R9
-.def	constante_10 = R10
+.def	compare_BLINK = R2
+.def	out_PORTD = R3
+.def	in_PORTC = R4
+.def	CONTADOR7 = R6
+.def	SALIDA7 = R7
+.def	max_dia = R8
+.def	contador_dia = R9
+.def	puntos_LED = R10
+.def	bandera_BLINK = R11
 
 .def	variable1 = R16
 .def	variable2 = R17
@@ -39,6 +38,8 @@
 .def	contador_mes = R20
 .def	out_PORTB = R21
 .def	bandera_ACCION = R22
+.def	ciclos_BLINK = R23
+.def	out_ALARMA = R24		// Salida para buzzer y led modo
 
 // Variables SRAM
 .dseg
@@ -51,6 +52,10 @@ dia_D:		.byte 1
 dia_U:		.byte 1
 mes_D:		.byte 1
 mes_U:		.byte 1
+Aminu_D:	.byte 1
+Aminu_U:	.byte 1
+Ahour_D:	.byte 1
+Ahour_U:	.byte 1
 
 // Codigo FLASH
 .cseg
@@ -77,7 +82,6 @@ SETUP:
 	OUT		SPL, R16
 	LDI		R16, HIGH(RAMEND)
 	OUT		SPH, R16
-
 	// Configuración del PRESCALER
 	LDI		R16, (1 << CLKPCE)
 	STS		CLKPR, R16				// Habilitar cambio de PRESCALER
@@ -107,10 +111,9 @@ SETUP:
     STS		PCICR, R16				// Habilita interrupciones en PORTC
     LDI		R16, (1 << PCINT11) | (1 << PCINT10) | (1 << PCINT9) | (1 << PCINT8)
     STS		PCMSK1, R16				// Habilita interrupciones en PC0 y PC1
-
 	//Inicializacion de variables
-	LDI		R16, 0x0A
-	MOV		constante_10, R16
+	LDI		R16, 0xFF
+	MOV		compare_BLINK, R16
 	LDI		R16, 0x80
 	MOV		puntos_LED, R16
 	CLR		ciclo_hora
@@ -123,10 +126,17 @@ SETUP:
 	STS		dia_D, R16
 	STS		mes_U, R16
 	STS		mes_D, R16
+	STS		Aminu_U, R16
+	STS		Aminu_D, R16
+	STS		Ahour_U, R16
+	STS		Ahour_D, R16
 	LDI		estado, 0x01
-	LDI		contador_mes, 0x00
+	LDI		contador_mes, 0xFF
 	LDI		R16, 0x01
 	MOV		contador_dia, R16
+	CLR		bandera_BLINK
+	CLR		ciclos_BLINK
+	CLR		out_ALARMA
 
 	CALL	NUEVO_MES
 	SEI
@@ -135,6 +145,7 @@ SETUP:
 //				LOOP INFINITO
 //****************************************
 MAIN:
+	CALL	SIGN_ESTADO
 	SBRC	bandera_ACCION, 0
 	CALL	INC_HORA
 	SBRC	bandera_ACCION, 1
@@ -143,11 +154,53 @@ MAIN:
 	CALL	INCREMENTO
 	SBRC	bandera_ACCION, 3
 	CALL	DECREMENTO
+	SBRC	bandera_ACCION, 5
+	CALL	comparar_ALARMA
 	RJMP	MAIN
 
 //****************************************
 //			SUB-RUTINAS GENERALES
 //****************************************
+SIGN_ESTADO:
+	SBRC	estado, 0
+	RJMP	estado_RELOJ
+	SBRC	estado, 1
+	RJMP	estado_FECHA
+	SBRC	estado, 2
+	RJMP	estado_RELOJ
+	SBRC	estado, 3
+	RJMP	estado_RELOJ
+	SBRC	estado, 4
+	RJMP	estado_FECHA
+	SBRC	estado, 5
+	RJMP	estado_FECHA
+	SBRC	estado, 6
+	RJMP	estado_ALARMA
+	SBRC	estado, 7
+	RJMP	estado_ALARMA
+	CPI		estado, 0x00
+	BREQ	estado_ALARMA_activa
+	estado_RELOJ:
+		SBI		PORTC, 4
+		CBI		PORTC, 5
+		CLR		out_ALARMA
+		RET
+	estado_FECHA:
+		CBI		PORTC, 4
+		SBI		PORTC, 5
+		CLR		out_ALARMA
+		RET
+	estado_ALARMA:
+		CBI		PORTC, 4
+		CBI		PORTC, 5
+		LDI		out_ALARMA, 0b00100000
+		RET
+	estado_ALARMA_activa:
+		CBI		PORTC, 4
+		CBI		PORTC, 5
+		LDI		out_ALARMA, 0b00110000
+		RET
+// ---------------- LOGICA DE DECREMENTO ----------------
 DECREMENTO_MINUTOS:
 	LDS		variable1, minu_U
 	CPI		variable1, 0x00
@@ -202,7 +255,16 @@ DECREMENTO:
 	BREQ	DECREMENTO_MES
 	CPI		estado, 0b00100000
 	BREQ	DECREMENTO_DIAS
+	CPI		estado, 0b01000000
+	BREQ	DEC_ALARMA_MIN
+	CPI		estado, 0b10000000
+	BREQ	DEC_ALARMA_HOUR
 	RET
+
+DEC_ALARMA_MIN:
+	RJMP	DECREMENTO_ALARMA_MIN
+DEC_ALARMA_HOUR:
+	RJMP	DECREMENTO_ALARMA_HOUR
 
 DECREMENTO_DIAS:
 	MOV		variable1, contador_dia
@@ -287,6 +349,7 @@ DECREMENTO_MES:
 		LDI		ZL, LOW(Tabla_maxDIAS<<1)	// Parte baja de la tabla
 		ADD		ZL, contador_mes			// Suma el contador al puntero Z
 		LPM		max_dia, Z					// Copia el valor del puntero
+		
 		RET
 // ---------------- LOGICA DE HORA ----------------
 INC_HORA:
@@ -350,11 +413,15 @@ INCREMENTO:
 	CPI		estado, 0b00100000
 	BREQ	AUMENTO_DIA
 	CPI		estado, 0b01000000
-	BREQ	AUMENTO_ALARMA
+	BREQ	INC_ALARMA_MIN
+	CPI		estado, 0b10000000
+	BREQ	INC_ALARMA_HOUR
 	RET
 
-AUMENTO_ALARMA:
-	RET
+INC_ALARMA_MIN:
+	RJMP	AUMENTO_ALARMA_MIN
+INC_ALARMA_HOUR:
+	RJMP	AUMENTO_ALARMA_HOUR
 
 // ---------------- LOGICA DE FECHA ----------------
 CAMBIO_DIA:
@@ -388,12 +455,12 @@ NUEVO_MES:
 	SBRC	estado, 5
 	RET
 
+	INC		contador_mes
 	LDI		ZH, HIGH(Tabla_maxDIAS<<1)	// Parte alta de Tabla7seg que esta en la Flash
 	LDI		ZL, LOW(Tabla_maxDIAS<<1)	// Parte baja de la tabla
 	ADD		ZL, contador_mes			// Suma el contador al puntero Z
 	LPM		max_dia, Z					// Copia el valor del puntero
-	INC		contador_mes
-
+	
 	LDS		variable1, mes_U
 	CPI		variable1, 0x02
 	BRNE	CONTINUAR_GUARDADO_MES
@@ -415,6 +482,122 @@ NUEVO_MES:
 		INC		variable1
 		STS		mes_U, variable1
 		RET
+
+// ---------------- LOGICA DE ALARMA ----------------
+AUMENTO_ALARMA_MIN:
+	LDS		variable1, Aminu_U
+	INC		variable1
+	CPI		variable1, 0x0A
+	BRNE	GUARDAR_AMINU_U
+	CLR		variable1
+	STS		Aminu_U, variable1
+	LDS		variable1, Aminu_D
+	INC		variable1
+	CPI		variable1, 0x06
+	BRNE	GUARDAR_AMINU_D
+	CLR		variable1
+	STS		Aminu_D, variable1
+	RET
+	GUARDAR_AMINU_U:
+		STS		Aminu_U, variable1
+		RET
+	GUARDAR_AMINU_D:
+		STS		Aminu_D, variable1
+		RET
+AUMENTO_ALARMA_HOUR:
+	LDS		variable1, Ahour_U
+	LDS		variable2, Ahour_D
+	INC		variable1
+	CPI		variable2, 0x02
+	BRNE	CONTINUAR_INC_AHORA
+	CPI		variable1, 0x04
+	BRNE	CONTINUAR_INC_AHORA
+	CLR		variable1
+	CLR		variable2
+	STS		Ahour_U, variable1
+	STS		Ahour_D, variable2
+	RET
+	CONTINUAR_INC_AHORA:
+		CPI		variable1, 0x0A
+		BRNE	GUARDAR_AHOUR
+		CLR		variable1
+		INC		variable2
+		GUARDAR_AHOUR:
+			STS		Ahour_U, variable1
+			STS		Ahour_D, variable2
+			RET
+
+DECREMENTO_ALARMA_MIN:
+	LDS		variable1, Aminu_U
+	CPI		variable1, 0x00
+	BRNE	SAVE_DEC_AMINU
+	LDI		variable1, 0x09
+	STS		Aminu_U, variable1
+	LDS		variable1, Aminu_D
+	CPI		variable1, 0x00
+	BRNE	SAVE_DEC_AMIND
+	LDI		variable1, 0x05
+	STS		Aminu_D, variable1
+	RET
+	SAVE_DEC_AMINU:
+		DEC		variable1
+		STS		Aminu_U, variable1
+		RET
+	SAVE_DEC_AMIND:
+		DEC		variable1
+		STS		Aminu_D, variable1
+		RET
+
+DECREMENTO_ALARMA_HOUR:
+	LDS		variable1, Ahour_U
+	CPI		variable1, 0x00
+	BRNE	SAVE_DEC_AHOURU
+	LDI		variable1, 0x09
+	STS		Ahour_U, variable1
+	LDS		variable1, Ahour_D
+	CPI		variable1, 0x00
+	BRNE	SAVE_DEC_AHOURD
+	LDI		variable1, 0x02
+	STS		Ahour_D, variable1
+	LDI		variable1, 0x03
+	STS		Ahour_U, variable1
+	RET
+	SAVE_DEC_AHOURU:
+		DEC		variable1
+		STS		Ahour_U, variable1
+		RET
+	SAVE_DEC_AHOURD:
+		DEC		variable1
+		STS		Ahour_D, variable1
+		RET
+
+comparar_ALARMA:
+	CBR		bandera_ACCION, 0b00100000
+	LDS		variable1, minu_U
+	LDS		variable2, Aminu_U
+	CPSE	variable1, variable2
+	RET 
+	LDS		variable1, minu_D
+	LDS		variable2, Aminu_D
+	CPSE	variable1, variable2
+	RET
+	LDS		variable1, hour_U
+	LDS		variable2, Ahour_U
+	CPSE	variable1, variable2
+	RET
+	LDS		variable1, hour_D
+	LDS		variable2, Ahour_D
+	CPSE	variable1, variable2
+	RET
+	CLR		variable1
+	STS		Aminu_U, variable1
+	STS		Aminu_D, variable1
+	STS		Ahour_U, variable1
+	STS		Ahour_D, variable1
+	SBR		out_ALARMA, 0b00010000
+	CBR		bandera_ACCION, 0b00010000
+	CLR		estado
+	RET
 
 // ---------------- LOGICA DE SALIDAS ----------------
 MOSTRAR_HORA: 
@@ -439,6 +622,17 @@ MOSTRAR_FECHA:
 	LDS		CONTADOR7, dia_D
 	RET
 
+MOSTRAR_ALARMA: 
+	SBRC	out_PORTB, 0				// Escoge salida de unidades o decenas
+	LDS		CONTADOR7, Aminu_U			// Copia valor en la salida del contador
+	SBRC	out_PORTB, 1
+	LDS		CONTADOR7, Aminu_D
+	SBRC	out_PORTB, 2
+	LDS		CONTADOR7, Ahour_U
+	SBRC	out_PORTB, 3
+	LDS		CONTADOR7, Ahour_D
+	RET
+
 //****************************************
 //		SUB-RUTINAS de INTERRUPCION
 //****************************************
@@ -449,6 +643,7 @@ TIMER0_ISR:
 	PUSH	R16
 	
 	IN		out_PORTB, PINB
+	ANDI	out_PORTB, 0x0F
 	LSL		out_PORTB
 	CPI		out_PORTB, 0x10
 	BRNE	SALIDA_PORTD
@@ -468,22 +663,47 @@ SALIDA_PORTD:
 	SBRC	estado, 4
 	CALL	MOSTRAR_FECHA
 	SBRC	estado, 5
-	CALL	MOSTRAR_FECHA	// Muestra la hora en el display
-
+	CALL	MOSTRAR_FECHA	// Muestra la fecha en el display
+	SBRC	estado, 6
+	CALL	MOSTRAR_ALARMA
+	SBRC	estado, 7
+	CALL	MOSTRAR_ALARMA	// Muestra la alarma en el display
+	CPI		estado, 0x00
+	BRNE	CONTINUAR_SALIDA
+	CALL	MOSTRAR_HORA
+	CONTINUAR_SALIDA:
 	LDI		ZH, HIGH(Tabla7seg<<1)	// Parte alta de Tabla7seg que esta en la Flash
 	LDI		ZL, LOW(Tabla7seg<<1)	// Parte baja de la tabla
 	ADD		ZL, CONTADOR7			// Suma el contador al puntero Z
 	LPM		SALIDA7, Z				// Copia el valor del puntero
 	
+	LDI		R16, 0x00
+	CP		bandera_BLINK, R16
+	BRNE	FIN_TIMER0
 	CPI		estado, 0b00000100
-	BREQ	APAGAR_DISPLAY34
+	BREQ	APAGAR_DISPLAY12
 	CPI		estado, 0b00001000
-	BREQ	APAGAR_DISPLAY12
-	CPI		estado, 0b00010000
 	BREQ	APAGAR_DISPLAY34
-	CPI		estado, 0b00100000
+	CPI		estado, 0b00010000
 	BREQ	APAGAR_DISPLAY12
+	CPI		estado, 0b00100000
+	BREQ	APAGAR_DISPLAY34
+	CPI		estado, 0b01000000
+	BREQ	APAGAR_DISPLAY12
+	CPI		estado, 0b10000000
+	BREQ	APAGAR_DISPLAY34
+	CPI		estado, 0x00
+	BREQ	estado_00
 	RJMP	FIN_TIMER0
+	estado_00:
+		CPI		out_PORTB, 0b00000001
+		BREQ	APAGAR_DISPLAY12
+		CPI		out_PORTB, 0b00000010
+		BREQ	APAGAR_DISPLAY12
+		CPI		out_PORTB, 0b00000100
+		BREQ	APAGAR_DISPLAY34
+		CPI		out_PORTB, 0b00001000
+		BREQ	APAGAR_DISPLAY34
 	APAGAR_DISPLAY12:
 		CPI		out_PORTB, 0b00000100
 		BREQ	FIN_TIMER0
@@ -501,9 +721,16 @@ SALIDA_PORTD:
 		MOV		SALIDA7, R16
 		RJMP	FIN_TIMER0
 FIN_TIMER0:
+	OR		out_PORTB, out_ALARMA
 	OR		out_PORTD, SALIDA7
 	OUT		PORTB, out_PORTB
 	OUT		PORTD, out_PORTD		// Muestra la salida en PORT D
+	INC		ciclos_BLINK
+	CPI		ciclos_BLINK, 100
+	BRNE	continuar_FIN_TIMER0
+	CLR		ciclos_BLINK
+	EOR		bandera_BLINK, compare_BLINK
+	continuar_FIN_TIMER0:
 	POP		R16
 	OUT		SREG, R16
 	POP		R17
@@ -521,18 +748,25 @@ TIMER1_ISR:
 	EOR		out_PORTD, puntos_LED
 	OUT		PORTD, out_PORTD
 	
+	CPI		estado, 0x00
+	BREQ	INCREMENTO_HORA
 	CPI		estado, 0b00000001
 	BREQ	INCREMENTO_HORA
 	CPI		estado, 0b00000010
 	BREQ	INCREMENTO_HORA
+	CPI		estado, 0b01000000
+	BREQ	INCREMENTO_HORA
+	CPI		estado, 0b10000000
+	BREQ	INCREMENTO_HORA
 	RJMP	FIN_TIMER1
-
 	INCREMENTO_HORA:		// Incremento de HORA
 	INC		ciclo_hora
 	CPI		ciclo_hora, max_ciclosT1
 	BRNE	FIN_TIMER1
 	CLR		ciclo_hora
 	SBR		bandera_ACCION, 0b00000001		// Activa el cambio de dia
+	SBRC	bandera_ACCION, 4				// Si alarma esta activa
+	SBR		bandera_ACCION, 0b00100000		// Activar comparar alarma
 FIN_TIMER1:
 	POP		R16
 	OUT		SREG, R16
@@ -548,25 +782,40 @@ PORTC_ISR:
 
 	IN		in_PORTC, PINC
 	SBRS	in_PORTC, 0
-	SBR		bandera_ACCION, 0b00000100
+	RJMP	INC_DISPLAY
 	SBRS	in_PORTC, 1
-	SBR		bandera_ACCION, 0b00001000
+	RJMP	DEC_DISPLAY
 	SBRS	in_PORTC, 2
-	LSL		estado
+	RJMP	INC_ESTADO
 	SBRS	in_PORTC, 3
-	LSR		estado
-
-	CPI		estado, 0b00000000
-	BREQ	UNDERFLOW_ESTADO
-	CPI		estado, 0b01000000
-	BREQ	OVERFLOW_ESTADO
+	RJMP	DEC_ESTADO
 	RJMP	FIN_PORTC_ISR
-	UNDERFLOW_ESTADO:
-		LDI		estado, 0b00100000
-		RJMP	FIN_PORTC_ISR
-	OVERFLOW_ESTADO:
+	INC_ESTADO:
+		LSL		estado
+		CPI		estado, 0x00
+		BRNE	FIN_PORTC_ISR
 		LDI		estado, 0b00000001
 		RJMP	FIN_PORTC_ISR
+	DEC_ESTADO:
+		LSR		estado
+		CPI		estado, 0x00
+		BRNE	FIN_PORTC_ISR
+		LDI		estado, 0b10000000
+		RJMP	FIN_PORTC_ISR
+	INC_DISPLAY:
+		SBR		bandera_ACCION, 0b00000100
+		RJMP	ALARMA_ON
+	DEC_DISPLAY:
+		SBR		bandera_ACCION, 0b00001000
+		RJMP	ALARMA_ON
+	ALARMA_ON:
+		CPI		estado, 0b01000000
+		BREQ	ACT_ALARMA
+		CPI		estado, 0b10000000
+		BREQ	ACT_ALARMA
+		RJMP	FIN_PORTC_ISR
+		ACT_ALARMA:
+		SBR		bandera_ACCION, 0b00010000
 FIN_PORTC_ISR:
 	POP		R16
 	OUT		SREG, R16
